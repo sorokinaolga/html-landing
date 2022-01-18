9eb22f81e281c1d632fa35155f04c74645f38317
sorokinaolga <sorokinaop@list.ru>
2022-01-18 17:17:14 +0300

    Подключен sprite-svg в основную задачу сборки

--- gulpfile.js
+++ gulpfile.js
@@ -41,7 +41,7 @@ export { svgSprive }
 const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
 
 // Основные задачи
-const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images));
+const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images, svgSprive));
 
 // Построение сценариев выполнения задач
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
